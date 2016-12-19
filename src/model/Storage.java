b11d8d711c02a6df66de622f1fb2253da935f9bd
Aurelia Delaune <aurelia.delaune@gmail.com>
2016-12-19 22:34:26 +0100

    Ajout de methodes et de javadoc dans d'autres entités

--- src/model/Storage.java
+++ src/model/Storage.java
@@ -9,4 +9,71 @@ package model;
  */
 public class Storage {
 
+	private int identifiantStorage;
+	private String url;
+	private int size;
+	
+	/**
+	 * default constructor for the Storage entity.
+	 */
+	public Storage() {
+		super();
+	}
+	
+	/**
+	 * full constructor for the Storage entity, with:
+	 * @param identifiantStorage
+	 * @param url
+	 * @param size
+	 */
+	public Storage(int identifiantStorage, String url, int size) {
+		super();
+		this.identifiantStorage = identifiantStorage;
+		this.url = url;
+		this.size = size;
+	}
+
+	/**
+	 * @return the identifiantStorage
+	 */
+	public int getIdentifiantStorage() {
+		return identifiantStorage;
+	}
+
+	/**
+	 * @param identifiantStorage the identifiantStorage to set
+	 */
+	public void setIdentifiantStorage(int identifiantStorage) {
+		this.identifiantStorage = identifiantStorage;
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public int getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
 }
